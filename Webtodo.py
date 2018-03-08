09a3289247036b583842d22751b8b9d39a5a48cf
Maciek Kalinowski <maciek.kalinowski@gmail.com>
2018-03-08 22:35:51 +0100

    Dodana obsługa bazy danych za pomocą SQLAlchemy zamiast pliku

--- Webtodo.py
+++ Webtodo.py
@@ -1,22 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pathlib import Path
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 
-def dodaj(var):
-    lista.append(var)
-    zapis()
+class Wiersz(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    wpis = db.Column(db.String(100), nullable=False)
 
-def usun(xxx):
-    lista.pop(int(xxx))
-    zapis()
 
-def zapis():
-    with open('ToDo2.txt','w') as plik :
-        for p in range(len(lista)):
-            #linijka = temp_list[p]
-            plik.write(lista[p]+'\n')
-    #print(lista)
+
 
 
 def odczyt():
@@ -30,13 +25,7 @@ def odczyt():
         file_cont[d]=file_cont[d][:-1]
     return file_cont
 
-# def wydruk():
-#     wynik = ''
-#     j = 0
-#     for i in lista:
-#         wynik += (str(j)+ ' - ' + i + ' ')
-#         j += 1
-#     return wynik
+
 
 
 lista = []
@@ -45,9 +34,11 @@ lista = odczyt()
 class TODO:
     list_path= Path('./lists')
 
-    def __init__(self, nazwa_pliku):
+    def __init__(self, nazwa_pliku, db):
         self.list = []
+        self.list_db = []
         self.nazwa_pliku = self.list_path / nazwa_pliku
+        self.db = db
 
     @classmethod
     def get_all_files(cls):
@@ -57,15 +48,28 @@ class TODO:
 
     def dodaj(self, element):
         self.list.append(element)
+
+    def dodaj_db(self, element):
+        wiersz= Wiersz(wpis=element)
+        self.db.session.add(wiersz)
+        self.db.session.commit()
         
     def usun(self, index):
         self.list.pop(index)
+
+    def usun_db(self, index):
+        wiersz= Wiersz.query.get(index)
+        self.db.session.delete(wiersz)
+        self.db.session.commit()
 
     def zapis(self):
         with open(self.nazwa_pliku,'w') as plik :
             for p in range(len(self.list)):
                 #linijka = temp_list[p]
                 plik.write(self.list[p]+'\n')
+
+    def zapis_db(self):
+        db.session.commit()
     
     def odczyt(self):
         """Wczytuje zawartosc pliku ToDo2.txt do listy i zwraca ta liste"""
@@ -77,9 +81,16 @@ class TODO:
         for d in range(len(file_cont)):
             file_cont[d]=file_cont[d][:-1]
         self.list = file_cont
-        
+
+    def odczyt_db(self):     
+        self.list_db = {i.id: i.wpis for i in Wiersz.query.all()}
+
+
     def show(self):
         return self.list
+
+    def show_db(self):
+        return self.list_db
 
 #todo=TODO()
 
@@ -94,26 +105,6 @@ def index():
 
 
 
-# @app.route('/formularz', methods=['POST', 'GET'])
-# def formularz():
-    
-#     Task = None
-#     ID = None
-#     #lista = odczyt()
-#     todo = TODO('ToDo2.txt')
-#     todo.odczyt()
-#     if request.method == 'POST':
-#         Task = request.form.get('Zadanie')
-#         ID = request.form.get('ID')
-#         #print('Task:{} ID:{}'.format(Task,ID))
-#         if Task not in ['',None]:
-#             todo.dodaj(Task)
-#         if ID not in ['',None]:
-#             todo.usun(int(ID))
-#         todo.zapis()
-    
-#         return render_template('form.html',zm=enumerate(todo.show()))
-#     return render_template('form.html',zm=enumerate(todo.show()))
 
 @app.route('/<nazwa_listy>',methods=['POST', 'GET'])
 def nazwa_listy(nazwa_listy):
@@ -121,17 +112,22 @@ def nazwa_listy(nazwa_listy):
     Task = None
     ID = None
     #lista = odczyt()
-    todo = TODO(nazwa_listy+'.txt')
-    todo.odczyt()
+    todo = TODO(nazwa_listy+'.txt', db)
+    #todo.odczyt()
+    todo.odczyt_db()
     if request.method == 'POST':
         Task = request.form.get('Zadanie')
-        ID = request.form.get('ID')
+        ID = request.form.get('ID')  
         #print('Task:{} ID:{}'.format(Task,ID))
         #if Task not in ['',None]:
         if Task :
-            todo.dodaj(Task)
+            #todo.dodaj(Task)
+            todo.dodaj_db(Task)
         if ID not in ['',None]:
-            todo.usun(int(ID))
-        todo.zapis()
-    
-    return render_template('form.html',zm=enumerate(todo.show()),ll=todo.get_all_files())   
+            #todo.usun(int(ID))
+            todo.usun_db(int(ID))
+        #todo.zapis()
+        todo.zapis_db()
+    todo.odczyt_db()
+    #return render_template('form.html',zm=enumerate(todo.show()),zm_db=todo.show_db().items(),ll=todo.get_all_files())
+    return render_template('form.html',zm_db=todo.show_db().items(),ll=todo.get_all_files())
