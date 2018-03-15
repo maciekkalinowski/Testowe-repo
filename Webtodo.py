e95e4c8440d28db8ea50ce9ea41d3bcae4d22e68
Maciek Kalinowski <maciek.kalinowski@gmail.com>
2018-03-15 19:50:36 +0100

    Rozdzielone klasy dla plików i bazy danych

--- Webtodo.py
+++ Webtodo.py
@@ -31,6 +31,89 @@ def odczyt():
 lista = []
 lista = odczyt()
 
+class ToDoDB:
+
+    def __init__(self, db):
+
+        self.list_db = []
+        self.db = db
+
+
+   
+    def dodaj(self, element):
+        wiersz= Wiersz(wpis=element)
+        self.db.session.add(wiersz)
+        self.db.session.commit()
+        
+   
+    def usun(self, index):
+        wiersz= Wiersz.query.get(index)
+        self.db.session.delete(wiersz)
+        self.db.session.commit()
+
+
+    def zapis(self):
+        db.session.commit()
+    
+
+    def odczyt(self):     
+        self.list_db = {i.id: i.wpis for i in Wiersz.query.all()}
+
+
+    def show(self):
+        return self.list_db
+
+#--------------------------------------   
+
+class ToDoFile:
+    list_path= Path('./lists')
+
+    def __init__(self, nazwa_pliku):
+        self.list = []
+
+        self.nazwa_pliku = self.list_path / nazwa_pliku
+
+
+    @classmethod
+    def get_all_files(cls):
+        #wynik = list(self.list_path.glob('*.txt'))
+        wynik = [x.stem for x in cls.list_path.glob('*.txt') ]
+        return wynik
+
+    def dodaj(self, element):
+        self.list.append(element)
+
+
+        
+    def usun(self, index):
+        self.list.pop(index)
+
+
+
+    def zapis(self):
+        with open(self.nazwa_pliku,'w') as plik :
+            for p in range(len(self.list)):
+                #linijka = temp_list[p]
+                plik.write(self.list[p]+'\n')
+
+
+    
+    def odczyt(self):
+        """Wczytuje zawartosc pliku ToDo2.txt do listy i zwraca ta liste"""
+
+        file_cont = []
+        #str_pom = ''
+        with open(self.nazwa_pliku) as plik :
+            file_cont = plik.readlines()
+        for d in range(len(file_cont)):
+            file_cont[d]=file_cont[d][:-1]
+        self.list = file_cont
+
+    def show(self):
+        return self.list
+
+#--------------------------------------
+
 class TODO:
     list_path= Path('./lists')
 
@@ -111,23 +194,24 @@ def nazwa_listy(nazwa_listy):
     
     Task = None
     ID = None
-    #lista = odczyt()
-    todo = TODO(nazwa_listy+'.txt', db)
-    #todo.odczyt()
-    todo.odczyt_db()
+
+    #todo = TODO(nazwa_listy+'.txt', db)
+    todo = ToDoDB(db)
+    #todo.odczyt_db()
+    todo.odczyt()
+    
     if request.method == 'POST':
         Task = request.form.get('Zadanie')
         ID = request.form.get('ID')  
-        #print('Task:{} ID:{}'.format(Task,ID))
-        #if Task not in ['',None]:
         if Task :
-            #todo.dodaj(Task)
-            todo.dodaj_db(Task)
+            #todo.dodaj_db(Task)
+            todo.dodaj(Task)
         if ID not in ['',None]:
-            #todo.usun(int(ID))
-            todo.usun_db(int(ID))
-        #todo.zapis()
-        todo.zapis_db()
-    todo.odczyt_db()
+            #todo.usun_db(int(ID))
+            todo.usun(int(ID))
+        #todo.zapis_db()
+        todo.zapis()
+    #todo.odczyt_db()
+    todo.odczyt()
     #return render_template('form.html',zm=enumerate(todo.show()),zm_db=todo.show_db().items(),ll=todo.get_all_files())
-    return render_template('form.html',zm_db=todo.show_db().items(),ll=todo.get_all_files())
+    return render_template('form.html',zm_db=todo.show().items())
