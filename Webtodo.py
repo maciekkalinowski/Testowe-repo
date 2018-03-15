523cb33ca9b89438a27c13fbcf26b275c26d385a
Maciek Kalinowski <maciek.kalinowski@gmail.com>
2018-03-15 20:13:37 +0100

    usunieta klasa TODO

--- Webtodo.py
+++ Webtodo.py
@@ -14,23 +14,6 @@ class Wiersz(db.Model):
 
 
 
-def odczyt():
-    """Wczytuje zawartosc pliku ToDo2.txt do listy i zwraca ta liste"""
-
-    file_cont = []
-    #str_pom = ''
-    with open('ToDo2.txt') as plik :
-        file_cont = plik.readlines()
-    for d in range(len(file_cont)):
-        file_cont[d]=file_cont[d][:-1]
-    return file_cont
-
-
-
-
-lista = []
-lista = odczyt()
-
 class ToDoDB:
 
     def __init__(self, db):
@@ -114,74 +97,13 @@ class ToDoFile:
 
 #--------------------------------------
 
-class TODO:
-    list_path= Path('./lists')
-
-    def __init__(self, nazwa_pliku, db):
-        self.list = []
-        self.list_db = []
-        self.nazwa_pliku = self.list_path / nazwa_pliku
-        self.db = db
-
-    @classmethod
-    def get_all_files(cls):
-        #wynik = list(self.list_path.glob('*.txt'))
-        wynik = [x.stem for x in cls.list_path.glob('*.txt') ]
-        return wynik
-
-    def dodaj(self, element):
-        self.list.append(element)
-
-    def dodaj_db(self, element):
-        wiersz= Wiersz(wpis=element)
-        self.db.session.add(wiersz)
-        self.db.session.commit()
-        
-    def usun(self, index):
-        self.list.pop(index)
-
-    def usun_db(self, index):
-        wiersz= Wiersz.query.get(index)
-        self.db.session.delete(wiersz)
-        self.db.session.commit()
-
-    def zapis(self):
-        with open(self.nazwa_pliku,'w') as plik :
-            for p in range(len(self.list)):
-                #linijka = temp_list[p]
-                plik.write(self.list[p]+'\n')
-
-    def zapis_db(self):
-        db.session.commit()
-    
-    def odczyt(self):
-        """Wczytuje zawartosc pliku ToDo2.txt do listy i zwraca ta liste"""
-
-        file_cont = []
-        #str_pom = ''
-        with open(self.nazwa_pliku) as plik :
-            file_cont = plik.readlines()
-        for d in range(len(file_cont)):
-            file_cont[d]=file_cont[d][:-1]
-        self.list = file_cont
-
-    def odczyt_db(self):     
-        self.list_db = {i.id: i.wpis for i in Wiersz.query.all()}
 
 
-    def show(self):
-        return self.list
 
-    def show_db(self):
-        return self.list_db
-
-#todo=TODO()
-
-#--------------------------------
 
 @app.route("/")
 def index():
-    return render_template('form.html',ll=TODO.get_all_files())
+    return render_template('form.html')
 
 
 
@@ -194,24 +116,19 @@ def nazwa_listy(nazwa_listy):
     
     Task = None
     ID = None
-
-    #todo = TODO(nazwa_listy+'.txt', db)
     todo = ToDoDB(db)
-    #todo.odczyt_db()
     todo.odczyt()
     
     if request.method == 'POST':
         Task = request.form.get('Zadanie')
         ID = request.form.get('ID')  
         if Task :
-            #todo.dodaj_db(Task)
             todo.dodaj(Task)
         if ID not in ['',None]:
-            #todo.usun_db(int(ID))
             todo.usun(int(ID))
-        #todo.zapis_db()
+
         todo.zapis()
-    #todo.odczyt_db()
+
     todo.odczyt()
     #return render_template('form.html',zm=enumerate(todo.show()),zm_db=todo.show_db().items(),ll=todo.get_all_files())
     return render_template('form.html',zm_db=todo.show().items())
